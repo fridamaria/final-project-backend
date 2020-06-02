d8423b1a2e3443a84b5304191dc6b1d71c7af128
Frida Eriksson <Frida.Eriksson@silf.se>
2020-06-02 13:34:23 +0200

    added imports, started setting up mongoUrl, mongoose connection and express-list-enpoints

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose'
+import crypto from 'crypto'
+import bcrypt from 'bcrypt-nodejs'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/finalAPI"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
 const port = process.env.PORT || 8080
 const app = express()
+
+const listEndpoints = require('express-list-endpoints')
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
+// Root endpoint listing endpoints and methods
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send(listEndpoints(app))
 })
 
 // Start the server
