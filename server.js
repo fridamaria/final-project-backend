46fea4487268f81558bee20c33c71bf7466c1d56
Kajsa Bootin <kajsa@Kajsas-Air.zyxel.com>
2020-06-03 14:33:55 +0200

    no changes

--- server.js
+++ server.js
@@ -1,163 +1,164 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import crypto from 'crypto'
-import bcrypt from 'bcrypt-nodejs'
-import Product from './models/product'
-import User from './models/user'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import crypto from "crypto";
+import bcrypt from "bcrypt-nodejs";
+import Product from "./models/product";
+import User from "./models/user";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/finalAPI"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/finalAPI";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
 const authenticateUser = async (req, res, next) => {
-  const user = await User.findOne({ accessToken: req.header('Authorization') })
+  const user = await User.findOne({ accessToken: req.header("Authorization") });
   if (user) {
-    req.user = user
-    next()
+    req.user = user;
+    next();
   } else {
-    res.status(401).json({ loggedOut: true })
+    res.status(401).json({ loggedOut: true });
   }
-}
+};
 
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
-const listEndpoints = require('express-list-endpoints')
+const listEndpoints = require("express-list-endpoints");
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 // Root endpoint listing endpoints and methods
-app.get('/', (req, res) => {
-  res.send(listEndpoints(app))
-})
+app.get("/", (req, res) => {
+  res.send(listEndpoints(app));
+});
 
-app.get('/products', async (req, res) => {
+app.get("/products", async (req, res) => {
   try {
-    const products = await Product.find()
-    res.status(200).json(products)
+    const products = await Product.find();
+    res.status(200).json(products);
   } catch (err) {
     res.status(400).json({
-      message: 'Invalid request.',
-      errors: err.error
-    })
+      message: "Invalid request.",
+      errors: err.error,
+    });
   }
-})
+});
 
-app.get('products/:productId', async (req, res) => {
-  const { productId } = req.params
+app.get("products/:productId", async (req, res) => {
+  const { productId } = req.params;
 
   try {
-    const product = await Product.findOne({ _id: productId })
-    res.status(200).json(product)
+    const product = await Product.findOne({ _id: productId });
+    res.status(200).json(product);
   } catch (err) {
     res.status(400).json({
-      message: 'Invalid request.',
-      errors: err.errors
-    })
+      message: "Invalid request.",
+      errors: err.errors,
+    });
   }
-})
+});
 
-app.post('/users', async (req, res) => {
-  const { name, email, password } = req.body
-  const encryptedPassword = bcrypt.hashSync(password)
+app.post("/users", async (req, res) => {
+  const { name, email, password } = req.body;
+  const encryptedPassword = bcrypt.hashSync(password);
 
   try {
-    const user = new User({ name, email, password: encryptedPassword })
-    const newUser = await user.save()
+    const user = new User({ name, email, password: encryptedPassword });
+    const newUser = await user.save();
 
     res.status(201).json({
-      message: 'User created.',
+      message: "User created.",
       userId: newUser._id,
-      accessToken: newUser.accessToken
-    })
+      accessToken: newUser.accessToken,
+    });
   } catch (err) {
     res.status(400).json({
-      message: 'Could not create user.',
-      errors: err.errors
-    })
+      message: "Could not create user.",
+      errors: err.errors,
+    });
   }
-})
+});
 
-app.get('/users/:userId', authenticateUser)
-app.get('/users/:userId', async (req, res) => {
-  const { userId } = req.params
+app.get("/users/:userId", authenticateUser);
+app.get("/users/:userId", async (req, res) => {
+  const { userId } = req.params;
 
   try {
-    const user = await User.findOne({ _id: userId })
-      .populate('orderHistory', 'createdAt')
+    const user = await User.findOne({ _id: userId }).populate(
+      "orderHistory",
+      "createdAt"
+    );
 
-    res.status(200).json(user)
+    res.status(200).json(user);
   } catch (err) {
     res.status(400).json({
-      message: 'Invalid request.',
-      errors: err.errors
-    })
+      message: "Invalid request.",
+      errors: err.errors,
+    });
   }
-})
+});
 
-app.put('/users/:userId', authenticateUser)
-app.put('/users/:userId', async (req, res) => {
-  const { userId } = req.params
-  const { name, email, street, postcode, city, phone } = req.body
+app.put("/users/:userId", authenticateUser);
+app.put("/users/:userId", async (req, res) => {
+  const { userId } = req.params;
+  const { name, email, street, postcode, city, phone } = req.body;
 
   try {
-    const user = await User.findOne({ _id: userId })
+    const user = await User.findOne({ _id: userId });
 
     if (user) {
-      user.name = name
-      user.email = email
-      user.street = street
-      user.postcode = postcode
-      user.city = city
-      user.phone = phone
-      user.save()
-      res.status(201).json({ message: `User ${userId} updated.` })
+      user.name = name;
+      user.email = email;
+      user.street = street;
+      user.postcode = postcode;
+      user.city = city;
+      user.phone = phone;
+      user.save();
+      res.status(201).json({ message: `User ${userId} updated.` });
     } else {
       res.status(404).json({
-        message: 'Could not update user.'
-      })
+        message: "Could not update user.",
+      });
     }
   } catch (err) {
     res.status(400).json({
-      message: 'Invalid request.',
-      errors: err.errors
-    })
+      message: "Invalid request.",
+      errors: err.errors,
+    });
   }
-})
+});
 
-app.delete('/users/:userId', authenticateUser)
-app.delete('/users/:userId', async (req, res) => {
-  const { userId } = req.params
+app.delete("/users/:userId", authenticateUser);
+app.delete("/users/:userId", async (req, res) => {
+  const { userId } = req.params;
   try {
-    await User.findByIdandRemove(userId)
-      .then(user => {
-        if (!user) {
-          res.status(404).json({ message: `Could not delete user ${userId}.` })
-        }
-        res.status(204).json({ message: `User ${userId} deleted.` })
-      })
+    await User.findByIdandRemove(userId).then((user) => {
+      if (!user) {
+        res.status(404).json({ message: `Could not delete user ${userId}.` });
+      }
+      res.status(204).json({ message: `User ${userId} deleted.` });
+    });
   } catch (err) {
     res.status(400).json({
-      message: 'Invalid request.',
-      errors: err.errors
-    })
+      message: "Invalid request.",
+      errors: err.errors,
+    });
   }
-})
+});
 
-app.post('/sessions', async (req, res) => {
-  const user = await User.findOne({ email: req.body.email })
+app.post("/sessions", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
   if (user && bcrypt.compareSync(req.body.password, user.password)) {
-    res.json({ userId: user._id, accessToken: user.accessToken })
+    res.json({ userId: user._id, accessToken: user.accessToken });
   } else {
-    res.status(400).json({ notFound: true })
+    res.status(400).json({ notFound: true });
   }
-})
+});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
